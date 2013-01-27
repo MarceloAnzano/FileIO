04e609dc726cf4071bd5982ec3ea031095d313b4
Marcelo <public.mranzano@gmail.com>
2013-01-27 12:37:20 +0800

    File Input Output in C++

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+
+using namespace std;
+
+int main()
+{
+    int x =0 ,c = 0, n;
+    int numbers[10000];
+    string line;
+    fstream myfile;
+    myfile.open("data10k.txt");
+    while(myfile.good())
+    {
+        getline(myfile,line);
+        stringstream(line)>>numbers[c];
+        c++;
+    }
+
+    for(c = 0; c<10000; c++)
+    {
+        cout<<numbers[c]<<endl;
+    }
+
+   /*for( n=0;n<11;n++)
+    {
+        if(numbers[n]>x)
+            {
+                x = numbers[n];
+            }
+    }
+    cout<<x;*/
+    myfile.close();
+
+    return 0;
+}
